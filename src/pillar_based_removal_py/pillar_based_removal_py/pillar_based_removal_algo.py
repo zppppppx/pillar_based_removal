486eaa6d71f24860a090d0826573df21ff0f38e9
zppppppx <3180105152@zju.edu.cn>
2024-06-18 17:03:32 +0000

    Added pillarize

--- src/pillar_based_removal_py/pillar_based_removal_py/pillar_based_removal_algo.py
+++ src/pillar_based_removal_py/pillar_based_removal_py/pillar_based_removal_algo.py
@@ -1,5 +1,17 @@
 from pillar_based_removal_node import PillarBasedRemoval
-from spconv.utils import Point2VoxelCPU3d
+from spconv.pytorch.utils import PointToVoxel as VoxelGenerator
 
-def pillarize():
-    pass
+
+def pillarize(self):
+    num_point_features = self.point_cloud_tensor_.shape[1]
+    self._pillar_generator = VoxelGenerator(
+        vsize_xyz=[self.resolution_, self.resolution_, 60],
+        coors_range_xyz=[self.lidar_ranges_[0], self.lidar_ranges_[1], -30, self.lidar_ranges_[2], self.lidar_ranges_[3], 30],
+        num_point_features=num_point_features,
+        max_num_points_per_voxel=2,
+        max_num_voxels=self.max_num_pillars_,
+        device=self.device_num_
+    )
+
+
+PillarBasedRemoval.pillarize = pillarize

e86e988f891963217719e02efe86f5c366ff64a6
zppppppx <3180105152@zju.edu.cn>
2024-06-22 04:51:22 +0000

    Changed .cpp to .cu

--- src/pillar_based_removal/src/pillar_based_removal_algo.cu
+++ src/pillar_based_removal/src/pillar_based_removal_algo.cu
@@ -1,4 +1,5 @@
-#include<pillar_based_removal/pillar_based_removal.h>
+#include <pillar_based_removal/pillar_based_removal.h>
+#include <pillar_based_removal/kernel.h>
 
 void PillarBasedRemoval::pillarize() {
     auto t0 = std::chrono::steady_clock::now();
@@ -30,6 +31,24 @@ void PillarBasedRemoval::pillarize() {
     // auto lo_ptr = pillars_lowest_.data_ptr<float>();
     // tv::ssprint(pillars_.shape(), pillars_highest_.shape(), hi_rw(1, 0), lo_rw(1, 0), pillars_rw(1, 0, 0), pillars_rw(1, 1, 0));
 
+    // tv::Tensor out_features; 
+    // tv::Tensor features;
+    // tv::Tensor indice_pairs; 
+    // tv::Tensor indice_pair_num;
+    // int num_activate_out; 
+    // std::uintptr_t stream;
+    // tv::Tensor indice_pair_mask;
+    // std::vector<int> dilation;
+    // std::vector<int> ksize;
+    // std::vector<int> input_dims;
+    // int batch_size;
+    // tv::Tensor indice_num_per_loc;
+    // tv::Tensor out_inds;
+    // tv::Tensor hashdata_v;
+    // tv::Tensor hashdata_k;
+    // tv::Tensor indices;
+    // SpconvOps::generate_subm_conv_inds(indices, hashdata_k, hashdata_v, indice_pairs, out_inds, indice_num_per_loc, batch_size, input_dims, ksize, dilation, indice_pair_mask, false, stream);
+
     auto p2p_grid_size = p2p.get_grid_size();
     grid_size_.assign(p2p_grid_size.begin(), p2p_grid_size.end());
     // tv::ssprint("grid_size", p2p_grid_size);
@@ -38,7 +57,7 @@ void PillarBasedRemoval::pillarize() {
     std::chrono::duration<double> time_used_ = std::chrono::duration_cast<std::chrono::duration<double>>(t1 - t0);
     if(verbose_)
         RCLCPP_INFO(get_logger(), "Time needed for pillarization is %f", time_used_);
-
+    checkCudaErrors(cudaStreamSynchronize(stream_));
 }
 
 void PillarBasedRemoval::removal_stage() {
@@ -50,13 +69,9 @@ void PillarBasedRemoval::removal_stage() {
     */
     tv::Tensor diff = tv::zeros(pillars_highest_.shape(), tv::type_v<float>, device_num_);
 
-    auto hi_rw = pillars_highest_.data_ptr<float>();
-    auto lo_rw = pillars_lowest_.data_ptr<float>();
-    auto diff_rw = diff.data_ptr<float>();
-
-    // for(size_t i = 0; i < pillars_highest_.dim(0); i++) {
-    //     diff_rw[(int)i] = hi_rw[(int)i] - lo_rw[(int)i];
-    // }
+    auto launcher = tv::cuda::Launch(diff.dim(0), stream_);
+    launcher(kernel::get_diff, diff.data_ptr<float>(), pillars_highest_.data_ptr<float>(), 
+            pillars_lowest_.data_ptr<float>(), diff.dim(0));
 
 
     int adjacent_pillars = (int) round(environment_radius_ / resolution_ - 0.5) * 2 + 1; // kernel size
@@ -70,34 +85,34 @@ void PillarBasedRemoval::removal_stage() {
     std::vector<int32_t> stride{1, 1, 1};
 
 
-    // // Set up maxpool working space
-    // std::vector<int32_t> input_dims(grid_size_.begin(), grid_size_.end());
-    // auto out_dims = SpconvOps::get_conv_output_size(input_dims, ksize, stride,
-    //                                               padding, dilation);
-    // int workspace_size = SpconvOps::get_indice_gen_workspace_size(
-    //         KV, pillars_.dim(0), out_inds_num_limit,
-    //         0, is_subm, false, false);
-    // tv::Tensor workspace = tv::empty({workspace_size}, tv::uint8, device_num_);
-    // auto ws_tensors = SpconvOps::get_indice_gen_tensors_from_workspace(
-    //         workspace.raw_data(), KV, pillars_.dim(0),
-    //         pillars_.dim(0), 0, is_subm,
-    //         false, false);
+    // Set up maxpool working space
+    std::vector<int32_t> input_dims(grid_size_.begin(), grid_size_.end());
+    auto out_dims = SpconvOps::get_conv_output_size(input_dims, ksize, stride,
+                                                  padding, dilation);
+    int workspace_size = SpconvOps::get_indice_gen_workspace_size(
+            KV, pillars_.dim(0), out_inds_num_limit,
+            0, is_subm, false, false);
+    tv::Tensor workspace = tv::empty({workspace_size}, tv::uint8, device_num_);
+    auto ws_tensors = SpconvOps::get_indice_gen_tensors_from_workspace(
+            workspace.raw_data(), KV, pillars_.dim(0),
+            pillars_.dim(0), 0, is_subm,
+            false, false);
 
-    // tv::Tensor pair = tv::empty({2, KV, pillars_.dim(0)}, tv::int32, device_num_);
-    // tv::Tensor indices_kernel_num = tv::zeros({KV}, tv::int32, device_num_);
-    // tv::Tensor out_inds = tv::empty({(int)pillars_.dim(0), (int)pillars_.ndim() + 1}, tv::int32, device_num_);
-    // // tv::ssprint("In removal stage:", pillars_.shape());
+    tv::Tensor pair = tv::empty({2, KV, pillars_.dim(0)}, tv::int32, device_num_);
+    tv::Tensor indices_kernel_num = tv::zeros({KV}, tv::int32, device_num_);
+    tv::Tensor out_inds = tv::empty({(int)pillars_.dim(0), (int)pillars_.ndim() + 1}, tv::int32, device_num_);
+    // tv::ssprint("In removal stage:", pillars_.shape());
 
-    // ws_tensors.insert({SPCONV_ALLOC_PAIR_FWD, pair});
-    // ws_tensors.insert(
-    //     {SPCONV_ALLOC_INDICE_NUM_PER_LOC, indices_kernel_num});
-    // ws_tensors.insert({SPCONV_ALLOC_OUT_INDICES, out_inds});
-    // StaticAllocator alloc(ws_tensors);
+    ws_tensors.insert({SPCONV_ALLOC_PAIR_FWD, pair});
+    ws_tensors.insert(
+        {SPCONV_ALLOC_INDICE_NUM_PER_LOC, indices_kernel_num});
+    ws_tensors.insert({SPCONV_ALLOC_OUT_INDICES, out_inds});
+    StaticAllocator alloc(ws_tensors);
 
     // // tv::ssprint(out_dims, workspace_size);
     // // padding with batch
-    // auto pillar_indices_padded = tv::zeros({pillar_indices_.dim(0), 4}, tv::int32, device_num_);
-
+    auto pillar_indices_padded = tv::zeros({pillar_indices_.dim(0), 4}, tv::int32, device_num_);
+    pillar_indices_padded.slice(1, 1, 4, 1, false, false).copy_2d_pitched_(pillar_indices_);
     // auto pillar_indices_padded_rw = pillar_indices_padded.tview<int32_t, 2>();
     // auto pillar_indices_rw = pillar_indices_.tview<int32_t, 2>();
 
@@ -107,34 +122,49 @@ void PillarBasedRemoval::removal_stage() {
     //     pillar_indices_padded_rw((int)i, 3) = pillar_indices_rw((int)i, 2);
     // }
 
-
-    // uint* stream = 0; // Don't use cuda stream
-    // int num_act_out_real = SpconvOps::get_indice_pairs(
-    //     alloc, pillar_indices_padded, batch_size, out_dims,
-    //     static_cast<int>(tv::gemm::SparseConvAlgo::kNative), ksize, stride,
-    //     padding, dilation, {0, 0, 0}, is_subm, false,
-    //     reinterpret_cast<std::uintptr_t>(stream), out_inds_num_limit,
-    //     pillar_indices_.dim(0));
+    
+    int num_act_out_real = SpconvOps::get_indice_pairs(
+        alloc, pillar_indices_padded, batch_size, out_dims,
+        static_cast<int>(tv::gemm::SparseConvAlgo::kNative), ksize, stride,
+        padding, dilation, {0, 0, 0}, is_subm, false,
+        reinterpret_cast<std::uintptr_t>(stream_), out_inds_num_limit,
+        pillar_indices_.dim(0));
 
     // // auto pair_rw = pair.tview<int, 3>();
     // // tv::ssprint("input/output num_indices", pillar_indices_.dim(0), num_act_out_real, pair_rw(0, 0, 0), pair_rw(0, 0, 1));
 
-    // tv::Tensor indices_pair_num = tv::zeros({KV}, tv::int32, device_num_);
-    // environment_lowest_ = tv::empty({pillars_lowest_.dim(0), 1}, tv::float32, device_num_);
+    tv::Tensor indices_pair_num = tv::zeros({KV}, tv::int32, device_num_);
+    environment_lowest_ = tv::empty({pillars_lowest_.dim(0), 1}, tv::float32, device_num_);
+
+    // tv::Tensor out_features; 
+    // tv::Tensor features;
+    // tv::Tensor indice_pairs; 
+    // tv::Tensor indice_pair_num;
+    // int num_activate_out; 
+    // std::uintptr_t stream;
+    // SpconvOps::indice_maxpool(out_features, features, indice_pairs, indice_pair_num, num_activate_out, stream);
+
+
+    // std::uintptr_t stream;
+    // stream = 0;
+    // spconvlib::spconv::csrc::sparse::all::SpconvOps::indice_maxpool(environment_lowest_, pillars_lowest_, 
+    //                             pair, indices_pair_num, num_act_out_real, reinterpret_cast<std::uintptr_t>(stream_));
+    // SpconvOps::maxpool_forward(environment_lowest_, pillars_lowest_, out_inds, 
+    //                             pillar_indices_padded, reinterpret_cast<std::uintptr_t>(stream_));
 
     // tv::check_shape(environment_lowest_, {-1, pillars_lowest_.dim(1)});
-
-    // auto indices_pair_num_ptr = indices_pair_num.data_ptr<int>();
-    // for (int i = 0; i < indices_pair_num.dim(0); ++i) {
-    //     int nhot = indices_pair_num_ptr[i];
+    // auto indices_pair_num_cpu = indices_pair_num.cpu();
+    // auto indices_pair_num_cpu_ptr = indices_pair_num_cpu.data_ptr<int>();
+    // for (int i = 0; i < indices_pair_num.dim(0); ++i){
+    //     int nhot = indices_pair_num_cpu_ptr[i];
     //     nhot = std::min(nhot, int(pair.dim(2)));
     //     if (nhot <= 0){
     //         continue;
     //     }
     //     auto inp_indices = pair[0][i].slice_first_axis(0, nhot);
     //     auto out_indices = pair[1][i].slice_first_axis(0, nhot);
-    //     IndiceMaxPoolCPU::forward(environment_lowest_, pillars_lowest_, out_indices, inp_indices);
-    // }   
+    //     IndiceMaxPool::forward(out_features, features, out_indices, inp_indices, stream);
+    // }
 
     
 
